8d44c910fdcd590b5b919ed9f2849180c713092d
Kerem Turer <oankistudio@gmail.com>
2026-08-21 07:29:39 +0300

    site guncellemesi

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -50,13 +50,13 @@ export default function Home() {
             <div className="flex flex-col gap-4">
               <div className="aspect-video w-full bg-white/5 relative">
                 <iframe
-                  src="https://www.youtube.com/embed/l27aeWB48QU?rel=0&modestbranding=1"
+                  src="https://www.youtube.com/embed/mfPxJq_ZToE?rel=0&modestbranding=1"
                   className="absolute top-0 left-0 w-full h-full"
                   allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                   allowFullScreen
                 ></iframe>
               </div>
-              <p className="text-sm text-gray-400 uppercase tracking-widest">Project Aelia</p>
+              <p className="text-sm text-gray-400 uppercase tracking-widest">Project Schneetraum Eis</p>
             </div>
 
             {/* Video 3 */}
@@ -76,13 +76,13 @@ export default function Home() {
             <div className="flex flex-col gap-4">
               <div className="aspect-video w-full bg-white/5 relative">
                 <iframe
-                  src="https://www.youtube.com/embed/Gm7gy8_276M?rel=0&modestbranding=1"
+                  src="https://www.youtube.com/embed/uYUAf3oFdAM?rel=0&modestbranding=1"
                   className="absolute top-0 left-0 w-full h-full"
                   allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                   allowFullScreen
                 ></iframe>
               </div>
-              <p className="text-sm text-gray-400 uppercase tracking-widest">Project LuxHertz</p>
+              <p className="text-sm text-gray-400 uppercase tracking-widest">Project Feather Airways</p>
             </div>
 
           </div>
