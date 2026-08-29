1022a82d797ac98f738dbd3ce61850d2f634cb47
Kerem Turer <oankistudio@gmail.com>
2026-08-29 08:20:47 +0300

    site guncellemesi

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -63,13 +63,13 @@ export default function Home() {
             <div className="flex flex-col gap-4">
               <div className="aspect-video w-full bg-white/5 relative">
                 <iframe
-                  src="https://www.youtube.com/embed/v=UJk3mAY8Ywk?rel=0&modestbranding=1"
+                  src="https://www.youtube.com/embed/7tLL-UvEKvo?rel=0&modestbranding=1"
                   className="absolute top-0 left-0 w-full h-full"
                   allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                   allowFullScreen
                 ></iframe>
               </div>
-              <p className="text-sm text-gray-400 uppercase tracking-widest">Ioni Dental</p>
+              <p className="text-sm text-gray-400 uppercase tracking-widest">Clear as Glass music video</p>
             </div>
 
             {/* Video 4 */}
